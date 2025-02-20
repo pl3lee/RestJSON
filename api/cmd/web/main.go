57e5e7d522e43541d8d2bdd2d36d8db8f37a9df5
pl3lee <billy.pl.lee@gmail.com>
2025-02-20 12:45:25 -0500

    made two apis due to cors issue

--- api/cmd/web/main.go
+++ api/cmd/web/main.go
@@ -20,12 +20,12 @@ func main() {
 	if err := godotenv.Load(); err != nil {
 		log.Printf("Cannot read .env file, this is normal when running in a docker container: %v\n", err)
 	}
-	port := os.Getenv("PORT")
-	clientURL := os.Getenv("CLIENT_URL")
-	dbUrl := os.Getenv("DB_URL")
-	sec := os.Getenv("AUTH_SECRET")
-	googleClientID, googleClientSecret := os.Getenv("GOOGLE_CLIENT_ID"), os.Getenv("GOOGLE_CLIENT_SECRET")
-	githubClientID, githubClientSecret := os.Getenv("GITHUB_CLIENT_ID"), os.Getenv("GITHUB_CLIENT_SECRET")
+	port := os.Getenv("WEB_PORT")
+	clientURL := os.Getenv("SHARED_CLIENT_URL")
+	dbUrl := os.Getenv("SHARED_DB_URL")
+	sec := os.Getenv("WEB_AUTH_SECRET")
+	googleClientID, googleClientSecret := os.Getenv("WEB_GOOGLE_CLIENT_ID"), os.Getenv("WEB_GOOGLE_CLIENT_SECRET")
+	githubClientID, githubClientSecret := os.Getenv("WEB_GITHUB_CLIENT_ID"), os.Getenv("WEB_GITHUB_CLIENT_SECRET")
 
 	cfg := config.Config{
 		Port:               port,
@@ -46,10 +46,6 @@ func main() {
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
 	r.Use(middleware.Timeout(60 * time.Second))
-	r.Use(cors.Handler(cors.Options{
-		AllowedOrigins: []string{"*"},
-		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-	}))
 
 	corsWeb := cors.Handler(cors.Options{
 		AllowedOrigins:   []string{cfg.ClientURL},
@@ -58,28 +54,9 @@ func main() {
 		AllowCredentials: true,
 		MaxAge:           300,
 	})
-	corsPublic := cors.Handler(cors.Options{
-		AllowedOrigins:   []string{"*"},
-		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type"},
-		AllowCredentials: false,
-		MaxAge:           300,
-	})
-	r.Group(func(r chi.Router) {
-		r.Use(corsWeb)
-		r.Route("/web", func(r chi.Router) {
-			r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-				utils.RespondWithJSON(w, http.StatusOK, "Hello world from web router!")
-			})
-		})
-	})
-	r.Group(func(r chi.Router) {
-		r.Use(corsPublic)
-		r.Route("/public", func(r chi.Router) {
-			r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-				utils.RespondWithJSON(w, http.StatusOK, "Hello world from public router!")
-			})
-		})
+	r.Use(corsWeb)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		utils.RespondWithJSON(w, http.StatusOK, "Hello world from web api!")
 	})
 
 	log.Printf("Listening on port %v", cfg.Port)
--- api/scripts/buildprod.sh
+++ api/scripts/buildprod.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o webjson-api
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o webjson-public-api ./cmd/public
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o webjson-web-api ./cmd/web
+
