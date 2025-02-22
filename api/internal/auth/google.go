04912702a24769b67b0c36a41070eb2e75887eb6
pl3lee <billy.pl.lee@gmail.com>
2025-02-21 21:14:46 -0500

    testing

--- api/internal/auth/google.go
+++ api/internal/auth/google.go
@@ -32,7 +32,7 @@ func (cfg *AuthConfig) exchangeCodeForTokenGoogle(code string) (*oauth2.Token, e
 	oauth2Config := &oauth2.Config{
 		ClientID:     cfg.GoogleClientID,
 		ClientSecret: cfg.GoogleClientSecret,
-		RedirectURL:  cfg.ClientURL + "/auth",
+		RedirectURL:  "https://staging.billylee.me/auth", // just for testing
 		Scopes: []string{
 			"https://www.googleapis.com/auth/userinfo.profile",
 			"https://www.googleapis.com/auth/userinfo.email",
