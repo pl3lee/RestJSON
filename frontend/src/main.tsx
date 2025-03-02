f5949beb49c6e7f29e828555428a410ce4b0b048
pl3lee <billy.pl.lee@gmail.com>
2025-03-01 22:12:48 -0500

    removed oauth from frontend code

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { GoogleOAuthProvider } from "@react-oauth/google";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Route, Routes } from "react-router";
 import { ThemeProvider } from "./components/theme-provider.tsx";
@@ -14,17 +13,15 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<QueryClientProvider client={queryClient}>
-			<GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
-				<ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-					<BrowserRouter>
-						<Routes>
-							<Route path="/" element={<Home />} />
-							<Route path="/auth" element={<Auth />} />
-							<Route path="/app" element={<App />} />
-						</Routes>
-					</BrowserRouter>
-				</ThemeProvider>
-			</GoogleOAuthProvider>
+			<ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+				<BrowserRouter>
+					<Routes>
+						<Route path="/" element={<Home />} />
+						<Route path="/auth" element={<Auth />} />
+						<Route path="/app" element={<App />} />
+					</Routes>
+				</BrowserRouter>
+			</ThemeProvider>
 		</QueryClientProvider>
 	</StrictMode>,
 );
