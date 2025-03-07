e9a4674080e97b0bc5cec99e1fc9d33c70536773
pl3lee <billy.pl.lee@gmail.com>
2025-03-06 21:38:03 -0500

    WIP: upload file to s3

--- api/internal/jsonfile/handlers.go
+++ api/internal/jsonfile/handlers.go
@@ -1,7 +1,9 @@
 package jsonfile
 
 import (
+	"io"
 	"net/http"
+	"os"
 
 	"github.com/google/uuid"
 	"github.com/pl3lee/webjson/internal/auth"
@@ -12,7 +14,28 @@ import (
 func (cfg *JsonConfig) HandlerCreateJson(w http.ResponseWriter, r *http.Request) {
 	userId := r.Context().Value(auth.UserIDContextKey).(uuid.UUID)
 
-	// TODO: create file in s3
+	// read json from request body
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		utils.RespondWithError(w, http.StatusBadRequest, "cannot read request body", err)
+		return
+	}
+	defer r.Body.Close()
+
+	// create json file
+	tempFile, err := os.CreateTemp("", "temp.json")
+	if err != nil {
+		utils.RespondWithError(w, http.StatusInternalServerError, "cannot create temp file", err)
+		return
+	}
+	defer os.Remove(tempFile.Name())
+	defer tempFile.Close()
+	if _, err := tempFile.Write(body); err != nil {
+		utils.RespondWithError(w, http.StatusInternalServerError, "cannot write to temp file", err)
+		return
+	}
+
+	// TODO: upload file to s3
 	file, err := cfg.Db.CreateNewJson(r.Context(), database.CreateNewJsonParams{
 		UserID:   userId,
 		FileName: "file",
