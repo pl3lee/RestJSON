db8dc05875e76eb35320e122c551a199da064bca
pl3lee <billy.pl.lee@gmail.com>
2025-03-08 20:54:10 -0500

    moved hook to the top

--- frontend/src/pages/App.tsx
+++ frontend/src/pages/App.tsx
@@ -1,12 +1,17 @@
 import { Button } from "@/components/ui/button";
 import { createJSON, getJSONFiles } from "@/lib/api";
+import { useQuery } from "@tanstack/react-query";
 import { useNavigate } from "react-router";
 import { useAuth } from "../hooks/useAuth";
-import { useQuery } from "@tanstack/react-query";
 
 export function App() {
 	const navigate = useNavigate();
 	const { user, isLoading: isLoadingUser, logout, isLoggedIn } = useAuth();
+	const { data: jsonFiles, isLoading: isLoadingFiles } = useQuery({
+		queryKey: ["jsonfiles"],
+		queryFn: getJSONFiles,
+		enabled: !!user,
+	});
 	if (isLoadingUser) {
 		return <div>Loading...</div>;
 	}
@@ -15,11 +20,6 @@ export function App() {
 		return null;
 	}
 
-	const { data: jsonFiles, isLoading: isLoadingFiles } = useQuery({
-		queryKey: ["jsonfiles"],
-		queryFn: getJSONFiles,
-		enabled: !isLoadingUser && isLoggedIn,
-	});
 	return (
 		<div className="flex flex-col gap-5">
 			Welcome {user.name}!
