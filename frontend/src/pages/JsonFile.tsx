852c7f07925396843879a906ae6de7aeaa75473c
pl3lee <billy.pl.lee@gmail.com>
2025-03-16 19:39:58 -0400

    changed title

--- frontend/src/pages/JsonFile.tsx
+++ frontend/src/pages/JsonFile.tsx
@@ -32,7 +32,7 @@ export function JsonFile() {
 
 	return (
 		<div className="flex flex-col gap-2">
-			<title>Edit JSON - RestJSON</title>
+			<title>{`${jsonMetadata!.fileName} - RestJSON`}</title>
 			<JsonFileTopbar
 				fileId={fileId!}
 				fileName={jsonMetadata!.fileName}
