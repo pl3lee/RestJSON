b8a98112d12e539e23148364bf42fb31f9105459
billy <billy.pl.lee@gmail.com>
2025-03-24 11:00:53 -0400

    skeleton to app page

--- frontend/src/pages/App.tsx
+++ frontend/src/pages/App.tsx
@@ -9,6 +9,7 @@ import { useState } from "react";
 import { Link, useNavigate } from "react-router";
 import { toast } from "sonner";
 import { useAuth } from "../hooks/useAuth";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export function App() {
 	const navigate = useNavigate();
@@ -39,9 +40,6 @@ export function App() {
 			toast.error(error.message);
 		},
 	});
-	if (isLoadingUser) {
-		return <div>Loading user...</div>;
-	}
 	if (!isLoggedIn) {
 		navigate("/auth");
 		return null;
@@ -54,64 +52,76 @@ export function App() {
 	return (
 		<div className="flex flex-col gap-5">
 			<title>Your JSON files - RestJSON</title>
-			<form
-				className="flex flex-row w-full gap-2"
-				onSubmit={(e) => {
-					e.preventDefault();
-					if (newFileName === "") {
-						toast.error("File name cannot be empty!");
-						return;
-					}
-					createMutation.mutate(newFileName);
-				}}
-			>
-				<Input
-					value={newFileName}
-					onChange={(e) => setNewFileName(e.target.value)}
-					placeholder="Enter new file name"
-				/>
-				<Button type="submit" disabled={createMutation.isPending}>
-					Create JSON File
-				</Button>
-			</form>
-			<div className="flex flex-col gap-2">
-				{!isLoadingFiles &&
-					jsonFiles &&
-					(jsonFiles.length === 0
-						? "No JSON files created yet."
-						: jsonFiles?.map((file) => (
-							<Card key={file.id}>
-								<CardContent className="flex flex-row justify-between items-center">
-									<Link
-										to={`/app/jsonfile/${file.id}`}
-										className="w-full h-full"
-									>
-										<div className="flex items-center gap-2">
-											<File className="h-5 w-5 text-primary" />
-											<span className="font-medium">{file.fileName}</span>
-										</div>
-										<div className="mt-2 text-sm text-foreground">
-											<div className="flex items-center gap-1">
-												<CalendarDays className="h-3.5 w-3.5" />
-												<span>Modified: {formatDate(file.modifiedAt)}</span>
-											</div>
-										</div>
-									</Link>
-									<Button
-										variant="destructive"
-										type="button"
-										onClick={(e) => {
-											e.stopPropagation();
-											deleteMutation.mutate(file.id);
-										}}
-										disabled={deleteMutation.isPending}
-									>
-										<Trash />
-									</Button>
-								</CardContent>
-							</Card>
-						)))}
-			</div>
+			{isLoadingUser ? (
+				<Skeleton className="h-8 w-full" />
+			) : (
+				<>
+					<form
+						className="flex flex-row w-full gap-2"
+						onSubmit={(e) => {
+							e.preventDefault();
+							if (newFileName === "") {
+								toast.error("File name cannot be empty!");
+								return;
+							}
+							createMutation.mutate(newFileName);
+						}}
+					>
+						<Input
+							value={newFileName}
+							onChange={(e) => setNewFileName(e.target.value)}
+							placeholder="Enter new file name"
+						/>
+						<Button type="submit" disabled={createMutation.isPending}>
+							Create JSON File
+						</Button>
+					</form>
+					<div className="flex flex-col gap-2">
+						{isLoadingFiles ? (
+							<Skeleton className="h-24 w-full" />
+						) : (
+							!isLoadingFiles &&
+							jsonFiles &&
+							(jsonFiles.length === 0
+								? "No JSON files created yet."
+								: jsonFiles?.map((file) => (
+									<Card key={file.id}>
+										<CardContent className="flex flex-row justify-between items-center">
+											<Link
+												to={`/app/jsonfile/${file.id}`}
+												className="w-full h-full"
+											>
+												<div className="flex items-center gap-2">
+													<File className="h-5 w-5 text-primary" />
+													<span className="font-medium">{file.fileName}</span>
+												</div>
+												<div className="mt-2 text-sm text-foreground">
+													<div className="flex items-center gap-1">
+														<CalendarDays className="h-3.5 w-3.5" />
+														<span>
+															Modified: {formatDate(file.modifiedAt)}
+														</span>
+													</div>
+												</div>
+											</Link>
+											<Button
+												variant="destructive"
+												type="button"
+												onClick={(e) => {
+													e.stopPropagation();
+													deleteMutation.mutate(file.id);
+												}}
+												disabled={deleteMutation.isPending}
+											>
+												<Trash />
+											</Button>
+										</CardContent>
+									</Card>
+								)))
+						)}
+					</div>
+				</>
+			)}
 		</div>
 	);
 }
