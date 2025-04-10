1554fa3392becfebca3933f9825e7b8d77bbe058
pl3lee <billy.pl.lee@gmail.com>
2025-04-09 22:46:56 -0400

    redis

--- api/internal/payment/config.go
+++ api/internal/payment/config.go
@@ -1,9 +1,13 @@
 package payment
 
-import "github.com/pl3lee/restjson/internal/database"
+import (
+	"github.com/pl3lee/restjson/internal/database"
+	"github.com/redis/go-redis/v9"
+)
 
 type JsonConfig struct {
 	Db        *database.Queries
 	BaseURL   string
 	ClientURL string
+	Rdb       *redis.Client
 }
